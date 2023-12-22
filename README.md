513accf0291417b28e2fe2ddc8fd941f8a8a0ed3
husanr <1090037611@qq.com>
2023-12-22 14:46:23 +0800

    fix: 更新api地址

--- README.md
+++ README.md
@@ -27,3 +27,5 @@ npm run lint
 ### Customize configuration
 
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+# 账号：coderwhy 密码: 123456
--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,16 +1,16 @@
-let BASE_URL = ""
-let BASE_NAME = ""
+const BASE_URL = "http://codercba.com:5000"
+const BASE_NAME = "aa"
 const TIME_OUT = 10000
 
-if (process.env.NODE_ENV === "development") {
-  BASE_URL = "/api"
-  BASE_NAME = "aaa"
-} else if (process.env.NODE_ENV === "production") {
-  BASE_URL = "http2"
-  BASE_NAME = "bbb"
-} else {
-  BASE_URL = "http3"
-  BASE_NAME = "ccc"
-}
+// if (process.env.NODE_ENV === "development") {
+//   BASE_URL = "/api"
+//   BASE_NAME = "aaa"
+// } else if (process.env.NODE_ENV === "production") {
+//   BASE_URL = "http2"
+//   BASE_NAME = "bbb"
+// } else {
+//   BASE_URL = "http3"
+//   BASE_NAME = "ccc"
+// }
 
 export { BASE_URL, BASE_NAME, TIME_OUT }
